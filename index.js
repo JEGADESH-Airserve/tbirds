780588e0df3cc260ac98f609af12dfd353ff3149
JEGADESH-Airserve <75108133+JEGADESH-Airserve@users.noreply.github.com>
2021-03-28 12:23:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -59,5 +59,6 @@ app.get('/home', verifymiddleware, (req, res) => {
 app.get('/logout', (req, res) => {
     res.clearCookie("token");
     res.redirect('/')
-})
-app.listen(3000);
+});
+const port = process.env.PORT || 3000
+app.listen(port);
